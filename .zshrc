5e03fa5d69daa8b054127fde1badfc6e5cf84a14
Alex Chavez <alex@alexchavez.net>
2020-05-26 00:36:21 -0700

    First bootstrap file

--- .zshrc
+++ .zshrc
@@ -1,66 +1,3 @@
-# If you come from bash you might have to change your $PATH.
-export PATH=$HOME/bin:/usr/local/bin:/usr/local/sbin:/Users/achavez/.local/bin:/Users/achavez/Library/Python/2.7/bin:$HOME/.bin:$PATH
-
-# Path to your oh-my-zsh installation.
-export ZSH=/Users/achavez/.oh-my-zsh
-
-# Uncomment the following line to use case-sensitive completion.
-# CASE_SENSITIVE="true"
-
-# Uncomment the following line to use hyphen-insensitive completion. Case
-# sensitive completion must be off. _ and - will be interchangeable.
-# HYPHEN_INSENSITIVE="true"
-
-# Uncomment the following line to disable bi-weekly auto-update checks.
-# DISABLE_AUTO_UPDATE="true"
-
-# Uncomment the following line to change how often to auto-update (in days).
-# export UPDATE_ZSH_DAYS=13
-
-# Uncomment the following line to disable colors in ls.
-# DISABLE_LS_COLORS="true"
-
-# Uncomment the following line to disable auto-setting terminal title.
-# DISABLE_AUTO_TITLE="true"
-
-# Uncomment the following line to enable command auto-correction.
-# ENABLE_CORRECTION="true"
-
-# Uncomment the following line to display red dots whilst waiting for completion.
-# COMPLETION_WAITING_DOTS="true"
-
-# Uncomment the following line if you want to disable marking untracked files
-# under VCS as dirty. This makes repository status check for large repositories
-# much, much faster.
-# DISABLE_UNTRACKED_FILES_DIRTY="true"
-
-# Uncomment the following line if you want to change the command execution time
-# stamp shown in the history command output.
-# The optional three formats: "mm/dd/yyyy"|"dd.mm.yyyy"|"yyyy-mm-dd"
-# HIST_STAMPS="mm/dd/yyyy"
-
-# Would you like to use another custom folder than $ZSH/custom?
-# ZSH_CUSTOM=/path/to/new-custom-folder
-
-# Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
-# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
-# Example format: plugins=(rails git textmate ruby lighthouse)
-# Add wisely, as too many plugins slow down shell startup.
-plugins=(
-  colored-man-pages
-  git
-  python
-  osx
-  zsh-syntax-highlighting
-)
-
-source $ZSH/oh-my-zsh.sh
-
-# Set name of the theme to load. Optionally, if you set this to "random"
-# it'll load a random theme each time that oh-my-zsh is loaded.
-# See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
-ZSH_THEME=""
-
 # User configuration
 
 # export MANPATH="/usr/local/man:$MANPATH"
@@ -80,24 +17,10 @@ export LANG='en_US.UTF-8'
 # export ARCHFLAGS="-arch x86_64"
 
 # ssh
-export SSH_KEY_PATH="~/.ssh/id_rsa"
+# export SSH_KEY_PATH="~/.ssh/id_rsa"
 
 # Import aliases
 . ~/.aliases
-. ~/.aliases-work
-
-# For asdf (Extendable version manager with support for Ruby, Node.jsm Elixir, Erlang & more)
-. $HOME/.asdf/asdf.sh
-. $HOME/.asdf/completions/asdf.bash
-
-test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
-
-# tabtab source for serverless package
-# uninstall by removing these lines or running `tabtab uninstall serverless`
-[[ -f /Users/achavez/.asdf/installs/nodejs/8.11.3/.npm/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.zsh ]] && . /Users/achavez/.asdf/installs/nodejs/8.11.3/.npm/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.zsh
-# tabtab source for sls package
-# uninstall by removing these lines or running `tabtab uninstall sls`
-[[ -f /Users/achavez/.asdf/installs/nodejs/8.11.3/.npm/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.zsh ]] && . /Users/achavez/.asdf/installs/nodejs/8.11.3/.npm/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.zsh
 
 # pipenv autocompletions
 if which pipenv > /dev/null; then
@@ -110,17 +33,41 @@ if which pyenv > /dev/null; then
     eval "$(pyenv init -)"
 fi
 
-# Set up tab completion for the go-jira CLI client
-if which jira > /dev/null; then
-    eval "$(jira --completion-script-bash)"
-fi
-
-# byobu
-export BYOBU_PREFIX=/usr/local
-
 # pure
 autoload -U promptinit; promptinit
 prompt pure
 
-# fzf (as a replacement/improvement to Ctrl+r)
+# set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/bin" ] ; then
+    PATH="$HOME/bin:$PATH"
+fi
+
+if [ -d "$HOME/.local/bin" ] ; then
+    PATH="$HOME/.local/bin:$PATH"
+fi
+
+# pyenv setup
+# export PATH="/usr/bin:$PATH"
+# export PATH="/home/alex/.pyenv/bin:$PATH"
+# eval "$(pyenv init -)"
+# eval "$(pyenv virtualenv-init -)"
+
+# Use neovim
+if type nvim > /dev/null 2>&1; then
+  export EDITOR=nvim
+  alias vim='nvim'
+fi
+
+# tilix
+if [ $TILIX_ID ] || [ $VTE_VERSION ]; then
+    source /etc/profile.d/vte.sh
+fi
+
+# Keep this at the bottom
+
+# prezto
+if [ ! -d "${HOME}/.zprezto" ]; then
+  source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
+fi
+
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
---
+++ bootstrap.sh
@@ -0,0 +1,234 @@
+#!/bin/bash
+
+set -eu
+
+export DEBIAN_FRONTEND=noninteractive
+
+UPGRADE_PACKAGES=${1:-none}
+
+if [ "${UPGRADE_PACKAGES}" != "none" ]; then
+  echo "==> Updating and upgrading packages ..."
+
+  # Add third party repositories
+  sudo add-apt-repository ppa:keithw/mosh-dev -y
+
+  sudo apt-get update
+  sudo apt-get upgrade -y
+fi
+
+sudo apt-get install -qq \
+  apache2-utils \
+  apt-transport-https \
+  bat \
+  build-essential \
+  bzr \
+  ca-certificates \
+  clang \
+  cmake \
+  curl \
+  direnv \
+  dnsutils \
+  docker.io \
+  fakeroot-ng \
+  gdb \
+  git \
+  git-crypt \
+  gnupg \
+  gnupg2 \
+  htop \
+  hugo \
+  ipcalc \
+  jq \
+  less \
+  libclang-dev \
+  liblzma-dev \
+  libpq-dev \
+  libprotoc-dev \
+  libsqlite3-dev \
+  libssl-dev \
+  libvirt-clients \
+  libvirt-daemon-system \
+  lldb \
+  locales \
+  man \
+  mosh \
+  mtr-tiny \
+  musl-tools \
+  ncdu \
+  neovim \
+  netcat-openbsd \
+  openssh-server \
+  pkg-config \
+  prettyping \
+  protobuf-compiler \
+  pwgen \
+  python \
+  python3 \
+  python3-flake8 \
+  python3-pip \
+  python3-setuptools \
+  python3-venv \
+  python3-wheel \
+  qemu-kvm \
+  qrencode \
+  quilt \
+  ripgrep \
+  shellcheck \
+  silversearcher-ag \
+  socat \
+  software-properties-common \
+  sqlite3 \
+  stow \
+  sudo \
+  tldr \
+  tmate \
+  tmux \
+  tree \
+  unzip \
+  wget \
+  zgen \
+  zip \
+  zlib1g-dev \
+  vim-gtk3 \
+  zsh \
+  --no-install-recommends \
+
+rm -rf /var/lib/apt/lists/*
+
+# install Go
+if ! [ -x "$(command -v go)" ]; then
+  export GO_VERSION="1.14.2"
+  wget "https://dl.google.com/go/go${GO_VERSION}.linux-amd64.tar.gz" 
+  tar -C /usr/local -xzf "go${GO_VERSION}.linux-amd64.tar.gz" 
+  rm -f "go${GO_VERSION}.linux-amd64.tar.gz"
+  export PATH="/usr/local/go/bin:$PATH"
+fi
+
+# install 1password
+if ! [ -x "$(command -v op)" ]; then
+  export OP_VERSION="v0.5.6-003"
+  curl -sS -o 1password.zip https://cache.agilebits.com/dist/1P/op/pkg/${OP_VERSION}/op_linux_amd64_${OP_VERSION}.zip
+  unzip 1password.zip op -d /usr/local/bin
+  rm -f 1password.zip
+fi
+
+# install protobuf
+if ! [ -x "$(command -v protoc)" ]; then
+  export PROTOBUF_VERSION="3.8.0"
+  mkdir -p protobuf_install 
+  pushd protobuf_install
+  wget https://github.com/protocolbuffers/protobuf/releases/download/v${PROTOBUF_VERSION}/protoc-${PROTOBUF_VERSION}-linux-x86_64.zip
+  unzip protoc-${PROTOBUF_VERSION}-linux-x86_64.zip
+  mv bin/protoc /usr/local/bin
+  mv include/* /usr/local/include/
+  popd
+  rm -rf protobuf_install
+fi
+
+# install tools
+if ! [ -x "$(command -v jump)" ]; then
+  echo " ==> Installing jump .."
+  export JUMP_VERSION="0.23.0"
+  wget https://github.com/gsamokovarov/jump/releases/download/v${JUMP_VERSION}/jump_${JUMP_VERSION}_amd64.deb
+  sudo dpkg -i jump_${JUMP_VERSION}_amd64.deb
+  rm -f jump_${JUMP_VERSION}_amd64.deb
+fi
+
+VIM_PLUG_FILE="${HOME}/.local/share/nvim/autoload/plug.vim"
+if [ ! -f "${VIM_PLUG_FILE}" ]; then
+  echo " ==> Installing vim plugins"
+  sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+  mkdir -p "${HOME}/.local/share/nvim/plugged"
+  pushd "${HOME}/.local/share/nvim/plugged"
+  git clone "https://github.com/AndrewRadev/splitjoin.vim"
+  git clone "https://github.com/ConradIrwin/vim-bracketed-paste"
+  git clone "https://github.com/Raimondi/delimitMate"
+  git clone "https://github.com/SirVer/ultisnips"
+  git clone "https://github.com/cespare/vim-toml"
+  git clone "https://github.com/corylanou/vim-present"
+  git clone "https://github.com/ekalinin/Dockerfile.vim"
+  git clone "https://github.com/elzr/vim-json"
+  git clone "https://github.com/fatih/vim-hclfmt"
+  git clone "https://github.com/fatih/vim-nginx"
+  git clone "https://github.com/fatih/vim-go"
+  git clone "https://github.com/hashivim/vim-hashicorp-tools"
+  git clone "https://github.com/junegunn/fzf.vim"
+  git clone "https://github.com/mileszs/ack.vim"
+  git clone "https://github.com/roxma/vim-tmux-clipboard"
+  git clone "https://github.com/plasticboy/vim-markdown"
+  git clone "https://github.com/scrooloose/nerdtree"
+  git clone "https://github.com/t9md/vim-choosewin"
+  git clone "https://github.com/tmux-plugins/vim-tmux"
+  git clone "https://github.com/tmux-plugins/vim-tmux-focus-events"
+  git clone "https://github.com/fatih/molokai"
+  git clone "https://github.com/tpope/vim-commentary"
+  git clone "https://github.com/tpope/vim-eunuch"
+  git clone "https://github.com/tpope/vim-fugitive"
+  git clone "https://github.com/tpope/vim-repeat"
+  git clone "https://github.com/tpope/vim-scriptease"
+  git clone "https://github.com/ervandew/supertab"
+  popd
+fi
+
+if [ ! -d "${HOME}/.fzf" ]; then
+  echo " ==> Installing fzf"
+  git clone https://github.com/junegunn/fzf "${HOME}/.fzf"
+  pushd "${HOME}/.fzf"
+  git remote set-url origin git@github.com:junegunn/fzf.git 
+  ${HOME}/.fzf/install --bin --64 --no-bash --no-zsh --no-fish
+  popd
+fi
+
+if [ ! -d "${HOME}/.tmux/plugins" ]; then
+  echo " ==> Installing tmux plugins"
+  git clone https://github.com/tmux-plugins/tpm "${HOME}/.tmux/plugins/tpm"
+  git clone https://github.com/tmux-plugins/tmux-open.git "${HOME}/.tmux/plugins/tmux-open"
+  git clone https://github.com/tmux-plugins/tmux-yank.git "${HOME}/.tmux/plugins/tmux-yank"
+  git clone https://github.com/tmux-plugins/tmux-prefix-highlight.git "${HOME}/.tmux/plugins/tmux-prefix-highlight"
+fi
+
+if [ ! -d "${HOME}/.zprezto" ]; then
+  echo "==> Installing prezto"
+  git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+  setopt EXTENDED_GLOB
+  for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+    ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+  done
+fi
+
+echo "==> Setting shell to zsh..."
+chsh -s /usr/bin/zsh
+
+echo "==> Creating dev directories"
+mkdir -p /root/code
+
+if [ ! -d /root/code/dotfiles ]; then
+  echo "==> Setting up dotfiles"
+  cd "/root/code"
+  git clone --recursive https://github.com/lxchavez/dotfiles.git
+
+  TMUX_CONFIG_FILE="${HOME}/.tmux/.tmux.conf"
+  if [ ! -f "${TMUX_CONFIG_FILE}" ]; then
+    echo "==> Also installing Oh My Tmux!..."
+    git clone https://github.com/gpakosz/.tmux.git
+    mkdir -p "${HOME}/.tmux"
+    ln -sfn $(pwd)/.tmux/.tmux.conf "${HOME}/.tmux/.tmux.conf"
+    cp $(pwd)/.tmux/.tmux.conf.local "${HOME}"
+  fi
+
+  cd "/root/code/dotfiles"
+  git remote set-url origin git@github.com:lxchavez/dotfiles.git
+
+  # the reason we dont't copy the files individually is, to easily push changes
+  # if needed
+  ln -sfn $(pwd)/.vimrc "${HOME}/.vimrc"
+  ln -sfn $(pwd)/.zshrc "${HOME}/.zshrc"
+  ln -sfn $(pwd)/.gitconfig "${HOME}/.gitconfig"
+  ln -sfn $(pwd)/.zprestorc "${HOME}/.zpreztorc"
+fi
+
+echo ""
+echo "==> Done!"
