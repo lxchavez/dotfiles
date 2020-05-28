1d7c0b6c66524e84b29a30f7d98a60cec4cadb68
Alex Chavez <alex@alexchavez.net>
2020-05-28 03:05:41 +0000

    awscli clean up

--- bootstrap.sh
+++ bootstrap.sh
@@ -255,6 +255,8 @@ if [ ! -x "$(command -v aws)" ]; then
   curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
   unzip awscliv2.zip
   sudo ./aws/install
+  rm -rf awscliv2.zip
+  rm -rf "${HOME}/awscliv2.zip"
 fi
 
 if [ ! -x "$(command -v terraform)" ]; then
